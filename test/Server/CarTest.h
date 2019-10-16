a345b6f90e5fe8c7d9b49c80fd7e00d80667b099
Constanza Frutos <constanza.frutos.ramos@gmail.com>
2019-10-15 22:08:27 -0300

    Added to tests to carTest

--- test/Server/CarTest.h
+++ test/Server/CarTest.h
@@ -3,8 +3,12 @@
 
 #include <cppunit/extensions/HelperMacros.h>
 
+#include "../../src/Server/Model/Car.h"
+
 class CarTest : public CppUnit::TestFixture {
 CPPUNIT_TEST_SUITE( CarTest );
+        CPPUNIT_TEST( testCreateCarInTheExitLine );
+        CPPUNIT_TEST( testCreateMultipleCarsInTheExitLineOneNextToEachOther );
         CPPUNIT_TEST( testStart );
         CPPUNIT_TEST( testIncreaseSpeed );
         CPPUNIT_TEST( testTurnRightWithHighManeuverabilityAndHighGrab );
@@ -20,6 +24,9 @@ CPPUNIT_TEST_SUITE_END();
 
 private:
     Car *car1, *car2, *car3;
+    float car_x_init = 0;//Ver cuales son las posiciones correctas en la pantalla.
+    float car_y_init = 0;
+
 public:
     void setUp(){
         car1 = new Car();
@@ -33,9 +40,38 @@ public:
         delete car3;
     }
 
-    void testStart(){}
+    void testCreateCarInTheExitLine(){
+        std::cout << "TESTS CAR\n";
 
-    void testIncreaseSpeed(){}
+        std::cout << "TEST create car in the middle of the track: \n";
+
+        CPPUNIT_ASSERT(car1->x == car_x_init);
+        CPPUNIT_ASSERT(car1->y == car_y_init);
+        std::cout << "OK\n";
+    }
+
+    void testCreateMultipleCarsInTheExitLineOneNextToEachOther(){
+        std::cout << "TEST create multiple cars in the middle of the track: \n";
+
+        CPPUNIT_ASSERT(car1->x == car_x_init);
+        CPPUNIT_ASSERT(car1->y == car_y_init);
+
+        //TODO: ver distancias entre autos
+        CPPUNIT_ASSERT(car2->x == car_x_init + 0.1);
+        CPPUNIT_ASSERT(car2->y == car_y_init);
+
+        CPPUNIT_ASSERT(car3->x == car_x_init + 0.2);
+        CPPUNIT_ASSERT(car3->y == car_y_init);
+        std::cout << "OK\n";
+    }
+
+    void testStart(){
+
+    }
+
+    void testIncreaseSpeed(){
+
+    }
 
     void testDecreaseSpeed(){}
 
