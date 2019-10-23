536068580a813ced29c44bb45598b795073025dc
Constanza Frutos <constanza.frutos.ramos@gmail.com>
2019-10-22 22:08:24 -0300

    Add box2d to car

--- src/Server/Model/Car.cpp
+++ src/Server/Model/Car.cpp
@@ -1,5 +1,66 @@
 #include "Car.h"
 
-Car::Car() : speed(0) {}
+void Car::_setShapeAndFixture(){
+    b2PolygonShape boxShape;
+    boxShape.SetAsBox(1,2);
+
+    b2FixtureDef boxFixtureDef;
+    boxFixtureDef.shape = &boxShape;
+    boxFixtureDef.density = 1; //Cuanta densidad?
+    _carBody->CreateFixture(&boxFixtureDef);
+}
+
+Car::Car(b2Body* carBody, size_t id) : _health(100), _previous_x(0), _previous_y(0) {
+    _carBody = carBody;
+    _carBody->SetLinearVelocity( b2Vec2( 0, 0 ) ); //not moving
+    _carBody->SetAngularVelocity( 0 );
+    _setShapeAndFixture();
+}
+
+
+
+void Car::start(){
+    b2Vec2 linear_velocity = b2Vec2(0, 5);
+    _carBody->SetLinearVelocity(linear_velocity);
+}
+
+void Car::_changeSpeed(float new_linear_velocity){
+    b2Vec2 linear_velocity = b2Vec2(0, new_linear_velocity);
+    _carBody->SetLinearVelocity(linear_velocity);
+}
+
+void Car::increaseSpeed(float to_increase){
+    float new_y_vel = _carBody->GetLinearVelocity().y + to_increase;
+    _changeSpeed(new_y_vel);
+}
+
+void Car::decreaseSpeed(float to_decrease){
+    float new_y_vel = _carBody->GetLinearVelocity().y - to_decrease;
+    _changeSpeed(new_y_vel);
+}
+
+void Car::resetCar(){
+    _health = 100;
+    //x should be middle, check if thereś already a car?
+    b2Vec2 position = b2Vec2(0, _previous_y);
+    _carBody->SetTransform(position, b2_pi/2); //See angle
+}
+
+const float Car::x(){
+    return _carBody->GetPosition().x;
+}
+
+const float Car::y(){
+    return _carBody->GetPosition().y;
+}
+
+const b2Vec2 Car::linearVelocity(){
+    return _carBody->GetLinearVelocity();
+}
+
+const float Car::speed(){
+    b2Vec2 linearVelocity = _carBody->GetLinearVelocity();
+    return linearVelocity.y;
+}
 
 Car::~Car(){}
--- src/Server/Model/Car.h
+++ src/Server/Model/Car.h
@@ -1,18 +1,33 @@
 #ifndef MICROMACHINES_CAR_H
 #define MICROMACHINES_CAR_H
 
+#include <Box2D/Box2D.h>
+#define DEGTORAD 0.0174532925199432957f
+#define RADTODEG 57.295779513082320876f
 
 class Car {
 private:
-    //TODO ask how to configure
-    int speed;
+    size_t id;
+    b2Body* _carBody;
+    int _health; //TODO health
+    float _previous_x, _previous_y;
 
-    int acceleration;
-    // What tipes are maneuvrability and grab?
-    int health;
+    void _setShapeAndFixture();
+    void _changeSpeed(float new_linear_velocity);
 
 public:
-    Car();
+    Car(b2Body* carBody, size_t id);
+
+    void start();
+    void increaseSpeed(float to_increase);
+    void decreaseSpeed(float to_decrease);
+
+    void resetCar();
+
+    const float x();
+    const float y();
+    const float speed();
+    const b2Vec2 linearVelocity();
 
     ~Car();
 };
