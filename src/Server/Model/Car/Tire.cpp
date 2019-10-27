86eb328f82f5a95b5411f4ebe337fd86207ce36f
Constanza Frutos <constanza.frutos.ramos@gmail.com>
2019-10-27 12:41:26 -0300

    More to the tire

--- src/Server/Model/Car/Tire.cpp
+++ src/Server/Model/Car/Tire.cpp
@@ -1,6 +1,8 @@
 #include "Tire.h"
 
-Tire::Tire(b2World* world){
+Tire::Tire(b2World* world, float maxForwardSpeed, float maxBackwardSpeed, float madDriveForce) :
+           _currentTraction(1), _maxForwardSpeed(maxForwardSpeed),
+           _maxBackwardSpeed(maxBackwardSpeed), _maxDriveForce(_maxDriveForce){
     _bodyDef.type = b2_dynamicBody;
     _body = world->CreateBody(&_bodyDef);
 
@@ -9,6 +11,16 @@ Tire::Tire(b2World* world){
     _body->CreateFixture(&polygonShape, 1);
 
     _body->SetUserData(this);
+}
+
+void Tire::addGroundArea(GroundAreaFUD* ga){
+    _groundArea = ga;
+    updateTraction();
+}
+
+void Tire::removeGroundArea(GroundAreaFUD* ga){
+    _groundArea = nullptr; // eliminar?
+    updateTraction();
 }
 
 b2Vec2 Tire::getLateralVelocity(){
@@ -21,15 +33,63 @@ b2Vec2 Tire::getForwardVelocity(){
     return b2Dot(currentForwardNormal, _body->GetLinearVelocity()) * currentForwardNormal;
 }
 
+void Tire::updateTraction(){
+    if (!_groundArea)
+        _currentTraction = 1;
+    else {
+        //find area with highest traction
+        _currentTraction = 0;
+        if (_groundArea->frictionModifier > _currentTraction)
+            _currentTraction = _groundArea->frictionModifier;
+    }
+}
+
+void Tire::updateDrive(int controlState){
+    //find desired speed
+    float desiredSpeed = 0;
+    switch ( controlState & (PRESS_UP|PRESS_DOWN) ) {
+        case PRESS_UP:   desiredSpeed = _maxForwardSpeed;  break;
+        case PRESS_DOWN: desiredSpeed = _maxBackwardSpeed; break;
+        default: return;//do nothing
+    }
+
+    //find current speed in forward direction
+    b2Vec2 currentForwardNormal = _body->GetWorldVector( b2Vec2(0,1) );
+    float currentSpeed = b2Dot( getForwardVelocity(), currentForwardNormal );
+
+    //apply necessary force
+    float force = 0;
+    if ( desiredSpeed > currentSpeed )
+        force = _maxDriveForce;
+    else if ( desiredSpeed < currentSpeed )
+        force = -_maxDriveForce;
+    else
+        return;
+    _body->ApplyForce( _currentTraction * force * currentForwardNormal, _body->GetWorldCenter(), true);
+}
+
+void Tire::updateTurn(int controlState){
+    float desiredTorque = 0;
+    switch ( controlState & (PRESS_LEFT|PRESS_RIGHT) ) {
+        case PRESS_LEFT:  desiredTorque = 15;  break;
+        case PRESS_RIGHT: desiredTorque = -15; break;
+        default: ;//nothing
+    }
+    _body->ApplyTorque( desiredTorque, true );
+}
+
 void Tire::updateFriction(){
+    float maxLateralImpulse = 2.5f;
     b2Vec2 impulse = _body->GetMass() * -getLateralVelocity();
-    _body->ApplyLinearImpulse(impulse, _body->GetWorldCenter(), true);
-    _body->ApplyAngularImpulse( 0.1f * _body->GetInertia() * -_body->GetAngularVelocity(), true);
+    if ( impulse.Length() > maxLateralImpulse )
+        impulse *= maxLateralImpulse / impulse.Length();
+    _body->ApplyLinearImpulse(_currentTraction * impulse, _body->GetWorldCenter(), true);
+    _body->ApplyAngularImpulse(_currentTraction * 0.1f * _body->GetInertia() * -_body->GetAngularVelocity(), true);
 
     b2Vec2 currentForwardNormal = getForwardVelocity();
     float currentForwardSpeed = currentForwardNormal.Normalize();
     float dragForceMagnitude = -2 * currentForwardSpeed;
-    _body->ApplyForce(dragForceMagnitude * currentForwardNormal, _body->GetWorldCenter(), true);
+    _body->ApplyForce(_currentTraction * dragForceMagnitude * currentForwardNormal, _body->GetWorldCenter(), true);
 }
 
 Tire::~Tire(){
--- src/Server/Model/Car/Tire.h
+++ src/Server/Model/Car/Tire.h
@@ -2,19 +2,66 @@
 #define MICROMACHINES_TIRE_H
 
 #include <Box2D/Box2D.h>
+#include "../Input.h"
+
+//types of fixture user data
+enum fixtureUserDataType {
+    FUD_CAR_TIRE,
+    FUD_GROUND_AREA
+};
+
+//a class to allow subclassing of different fixture user data
+class FixtureUserData {
+    fixtureUserDataType m_type;
+protected:
+    FixtureUserData(fixtureUserDataType type) : m_type(type) {}
+public:
+    virtual fixtureUserDataType getType() { return m_type; }
+    virtual ~FixtureUserData() {}
+};
+
+//class to allow marking a fixture as a car tire
+class CarTireFUD : public FixtureUserData {
+public:
+    CarTireFUD() : FixtureUserData(FUD_CAR_TIRE) {}
+};
+
+//class to allow marking a fixture as a ground area
+class GroundAreaFUD : public FixtureUserData {
+public:
+    float frictionModifier;
+    bool outOfCourse;
+
+    GroundAreaFUD(float fm, bool ooc) : FixtureUserData(FUD_GROUND_AREA) {
+        frictionModifier = fm;
+        outOfCourse = ooc;
+    }
+};
+
 
 class Tire {
 private:
     b2BodyDef _bodyDef;
     b2Body* _body;
 
-    float _maxSpeed;
+    float _maxForwardSpeed;
+    float _maxBackwardSpeed;
+    float _maxDriveForce;
+    GroundAreaFUD* _groundArea;
+    float _currentTraction;
 
 public:
-    Tire(b2World* world);
+    Tire(b2World* world, float maxForwardSpeed, float maxBackwardSpeed, float madDriveForce);
+
+    void addGroundArea(GroundAreaFUD* ga);
+    void removeGroundArea(GroundAreaFUD* ga);
+
     b2Vec2 getLateralVelocity();
     b2Vec2 getForwardVelocity();
+    void updateTraction();
     void updateFriction();
+    void updateDrive(int controlState);
+    void updateTurn(int controlState);
     ~Tire();
 };
 
