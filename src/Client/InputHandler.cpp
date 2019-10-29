b562ba7efdc0dd536c5eb7722367318e209eb75c
sberoch <sberoch@gmail.com>
2019-10-29 14:17:54 -0300

    fix handler to send last event, not all

--- src/Client/InputHandler.cpp
+++ src/Client/InputHandler.cpp
@@ -15,56 +15,54 @@ bool InputHandler::done() {
 }
 
 void InputHandler::handle() {
-	while (SDL_PollEvent(&event)) {
-		if (event.type == SDL_QUIT) {
-			_done = true;
-			sendQueue.push("clientQuit"); //TODO: especificar quien se va
-
-		} else if (event.type == SDL_KEYDOWN) {
-			SDL_KeyboardEvent& keyEvent = (SDL_KeyboardEvent&) event;
-			switch (keyEvent.keysym.sym) {
-				case SDLK_a: {
-					//Turn left
-					sendQueue.push("a");
-					break;
-				}
-				case SDLK_d: {
-					//Turn right
-					sendQueue.push("d");
-					break;
-				}
-				case SDLK_w: {
-					//Accelerate
-					audio.playEffect(SFX_CAR_ENGINE);
-					sendQueue.push("w");
-					break;
-				}
-				case SDLK_F11: {
-					if (fullscreen) {
-						window.setFullscreen(false);
-						fullscreen = false;
-					} else {
-						window.setFullscreen(true);
-						fullscreen = true;
-					}
-					break;
-				}
-
-				//Mock
-				case SDLK_k: {
-					audio.playEffect(SFX_CAR_EXPLOSION);
-					break;
-				}
-				case SDLK_l: {
-					audio.playEffect(SFX_BUTTON);
-					break;
-				}
+	SDL_PollEvent(&event);
+	if (event.type == SDL_QUIT) {
+		_done = true;
+		sendQueue.push("clientQuit"); //TODO: especificar quien se va
+	} else if (event.type == SDL_KEYDOWN) {
+		SDL_KeyboardEvent& keyEvent = (SDL_KeyboardEvent&) event;
+		switch (keyEvent.keysym.sym) {
+			case SDLK_a: {
+				//Turn left
+				sendQueue.push("a");
+				break;
 			}
-		} else if (event.type == SDL_KEYUP) {
-			SDL_KeyboardEvent& keyEvent = (SDL_KeyboardEvent&) event;
-			if (keyEvent.keysym.sym == SDLK_w) {
-				audio.stopEffect(SFX_CAR_ENGINE);
+			case SDLK_d: {
+				//Turn right
+				sendQueue.push("d");
+				break;
 			}
+			case SDLK_w: {
+				//Accelerate
+				audio.playEffect(SFX_CAR_ENGINE);
+				sendQueue.push("w");
+				break;
+			}
+			case SDLK_F11: {
+				if (fullscreen) {
+					window.setFullscreen(false);
+					fullscreen = false;
+				} else {
+					window.setFullscreen(true);
+					fullscreen = true;
+				}
+				break;
+			}
+
+			//Mock
+			case SDLK_k: {
+				audio.playEffect(SFX_CAR_EXPLOSION);
+				break;
+			}
+			case SDLK_l: {
+				audio.playEffect(SFX_BUTTON);
+				break;
+			}
+		}
+	} else if (event.type == SDL_KEYUP) {
+		SDL_KeyboardEvent& keyEvent = (SDL_KeyboardEvent&) event;
+		if (keyEvent.keysym.sym == SDLK_w) {
+			audio.stopEffect(SFX_CAR_ENGINE);
 		}
 	}
 }
