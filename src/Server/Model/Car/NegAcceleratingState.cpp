2116775e856eb1aab00dd360369a184b18ed601f
Constanza Frutos <constanza.frutos.ramos@gmail.com>
2019-11-05 17:24:49 -0300

    Delete states

--- src/Server/Model/Car/NegAcceleratingState.cpp
+++
@@ -1,9 +0,0 @@
-#include "NegAcceleratingState.h"
-
-CarMovingState* NegAcceleratingState::handleInput(Car& car, const InputEnum& input){
-    return makeMovingState(input);
-}
-
-void NegAcceleratingState::update(Car& car){
-    car.desaccelerate();
-}
