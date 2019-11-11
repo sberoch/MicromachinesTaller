7a2efb3f008d4baea59eb841c74e54a7a640f723
Constanza Frutos <constanza.frutos.ramos@gmail.com>
2019-11-10 22:34:06 -0300

    Add extern command

--- src/Server/serverMain.cpp
+++ src/Server/serverMain.cpp
@@ -4,6 +4,10 @@
 #include "Model/GameThread.h"
 #include <thread>
 
+extern "C" {
+#include "mods/plugin.h"
+}
+
 int main(int argc, char const *argv[]) {
     try {
         std::shared_ptr<Configuration> configuration(std::make_shared<Configuration>());
