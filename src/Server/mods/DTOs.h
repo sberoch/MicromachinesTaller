b72b12dded14688e354ed8e7d709050c8d92c639
Constanza Frutos <constanza.frutos.ramos@gmail.com>
2019-11-10 22:40:58 -0300

    Fix compiling errors

--- src/Server/mods/DTOs.h
+++ src/Server/mods/DTOs.h
@@ -1,21 +1,21 @@
 #ifndef MICROMACHINES_DTOS_H
 #define MICROMACHINES_DTOS_H
 
-struct CarDTO {
+typedef struct CarDTO {
     float x;
     float y;
     float angle;
     float maxForwardSpeed;
     float lapsCompleted;
     size_t health;
-};
+} CarDTO_t;
 
 
 
 struct WorldDTO {
-    CarDTO cars[10];
+    CarDTO_t cars[10];
 
 };
 
 
-#endif //MICROMACHINES_DTOS_H
+#endif
--- src/Server/mods/plugin.c
+++ src/Server/mods/plugin.c
@@ -10,11 +10,11 @@ int plugin_func(void) {
     return 42;
 }
 
-void plugin_car_damaged(CarDTO* cars[], size_t size){
+void plugin_car_damaged(CarDTO_t* cars[], size_t size){
     for (size_t i=0; i<size; ++i){
         if (cars[i]->health < MAX_HEALTH/2) {
             cars[i]->maxForwardSpeed -= TO_DECREMENT_IF_HALF_HEALTH;
-        } else if (cars[i].health < MAX_HEALTH) {
+        } else if (cars[i]->health < MAX_HEALTH) {
             cars[i]->maxForwardSpeed -= TO_DECREMENT_IF_MORE_THAN_HALF;
         }
     }
